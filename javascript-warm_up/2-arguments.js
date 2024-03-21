730a690a61c1cedad7c2ad6897dd61c22e219f10
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-21 12:03:44 -0500

    more than [3]

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length < 1) {
+if (process.argv === 2) {
     console.log('No argument');
-} else if (process.argv.length === 1) {
-        console.log('Argument found');
+} else if (process.argv === 3) {
+    console.log('Argument found');
 } else {
     console.log('Arguments found');
 }
