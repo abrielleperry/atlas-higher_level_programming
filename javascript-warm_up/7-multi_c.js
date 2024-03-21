a45897e576e2c801474dc22108c295b468f056f5
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-21 13:13:58 -0500

    for const

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const str = 'C is fun';
 const num = process.argv[2];
-for (str.repeat(num)) {
-  console.log(str)
+for (const str = 'C is fun') {
+  console.log(str.repeat(num);
 }
