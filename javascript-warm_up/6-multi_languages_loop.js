c2b8aaaca9ccf2832f84811298b7b81e98b7700b
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-21 12:37:04 -0500

    for loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+const myVar 
+for myVar {
+    console.log(myVar)
+}= 'C is fun\nPython is cool\nJavaScript is amazing';
+
