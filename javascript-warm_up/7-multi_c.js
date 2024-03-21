ff10f8f4bd3e6143d9048861175f905d361d08ac
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-21 13:15:54 -0500

    iterate num times

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const num = process.argv[2];
-for (const str = 'C is fun') {
-  console.log(str.repeat(num);
+const str = 'C is fun';
+for (i = 0; i < num; i++) {
+  console.log(str);
 }
