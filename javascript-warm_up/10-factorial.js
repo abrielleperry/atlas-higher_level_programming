1a79e48f7dea71b871ddd87f08c9611f0882dbea
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-22 15:38:13 -0500

    recursive

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 function factorial (n) {
-if (!Number.isInteger(n) || n <= 1) {
-  return 1;
-}
+  if (!Number.isInteger(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
