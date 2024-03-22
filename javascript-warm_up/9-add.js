c04de0b121208597881e62a6433944de0da39bad
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-22 11:01:02 -0500

    process not parse

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const a = parse.argv[2];
-const b = parse.argv[3];
+const a = process.argv[2];
+const b = process.argv[3];
 function add (a, b) {
+} if (NaN(a), (b)) {
+  console.log('NaN');
+} else {
+  return a + b;
 }
