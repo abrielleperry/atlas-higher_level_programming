056420c454b4cbb009b111ada3868fe6ed2bd62a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-22 16:14:22 -0500

    sbn func

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-function secondBiggest (array) {
+function secondBiggestNum (array) {
   if (array.length <= 1) {
     return 0;
   }
   array.sort(function (a, b) { return a - b; });
   return array[array.length - 2];
 }
-console.log(secondBiggest(process.argv.slice(2).map(Number)));
+console.log(secondBiggestNum(process.argv.slice(2).map(Number)));
 // convert arguments to ints
