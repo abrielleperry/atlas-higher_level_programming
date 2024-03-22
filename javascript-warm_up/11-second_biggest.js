af4c1b38764341e6eabf84ea88f30b37e835477d
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-22 16:05:49 -0500

    array

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-function secondBiggest (arr) {
-  if (arr.length <= 1) {
+function secondBiggest (array) {
+  if (array.length <= 1) {
     return 0;
   }
+  array.sort(function (a, b) { return a - b; });
+  return array[array.length - 2];
 }
+console.log(secondBiggest(process.argv.slice(2).map(Number)));
+
