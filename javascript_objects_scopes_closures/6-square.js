51cb56a5cec3e0da849a6ed4994d92a1eb30eb6c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-25 17:52:16 -0500

    keeping class from 5

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 const Square = require('./5-square.js');
 class Square extends Square {
-  constructor (charPrint) {
-    super(size, size);
+    constructor(size) {
+      super(size, size);
+    }
   }
 }
 module.exports = Square;
