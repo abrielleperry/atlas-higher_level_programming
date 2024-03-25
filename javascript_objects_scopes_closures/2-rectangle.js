f835edc3bda0a530ac64d5e978c09ef2e68d94fb
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-25 14:01:03 -0500

    logical op

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-
-    if (this.width <= 0) {
-      return {};
-    }
-    if (this.height <= 0) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
       return {};
     }
   }
