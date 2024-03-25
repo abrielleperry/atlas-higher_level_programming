c617250a37ad6942ea485667482d94b858a01bba
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-25 17:37:55 -0500

    add notes

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./4-rectangle'); // import Rectangle class from 4 file
 class Square extends Rectangle {
   constructor (size) {
-    super(size, size);
+    super(size, size); // calls the parent class(Rec) constructor with size as w and h
   }
 }
-
 module.exports = Square;
