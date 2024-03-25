9bde7b194132e52b27ee3a349ac54c44534ee07c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-25 18:12:09 -0500

    check if undef first

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -6,11 +6,11 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
-      if (c === undefined) {
-        c = 'X';
-      }
     }
   }
 }
