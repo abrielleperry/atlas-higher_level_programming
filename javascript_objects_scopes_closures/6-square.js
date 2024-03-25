969c0ef2b56058fbb6411cb4ef02dccfe62137d7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-25 17:45:59 -0500

    import and extend sq

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
+const Square = require('./5-square.js');
+class Square extends Square {
   constructor (size) {
     super(size, size);
   }
