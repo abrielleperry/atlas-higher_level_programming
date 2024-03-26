e0d3e2bb74cd141ff4d0d71ea65b977fa9ff2113
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-26 11:00:47 -0500

    if item = search elem

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -2,6 +2,8 @@
 exports.nbOccurences = function (list, searchElement) {
   let counter = 0;
   for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      counter++;
     }
   }
 
