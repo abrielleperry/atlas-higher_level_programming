720cea52735936119fb73a851284099b86d45ba3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-26 14:21:13 -0500

    tm length

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
+let counter = 0;
 exports.logMe = function (item) {
-  let counter = 0;
   counter += item.length;
-  console.log('{counter}:  + {item[item.length]}');
+  console.log({counter} + ':' + {item});
 }
