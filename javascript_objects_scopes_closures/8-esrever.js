81363602931188cec6771af13c32d8764dd1bc57
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-26 11:36:49 -0500

    add personal notes

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,3 +2,10 @@
 exports.esrever = function (list) {
     return list.reduce((newList, item) => [item].concat(newList), []);
   };
+
+
+  // reduce func processes the array from left to right
+  // newList is the accumulator and item is the current value
+  // concat function is used to add current item to newList array
+  // the initial value of newList is an empty array
+  
