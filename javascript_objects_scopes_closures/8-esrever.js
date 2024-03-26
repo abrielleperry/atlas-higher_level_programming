2a7619eb6bf193a29d73b55dd93ea8d21c0b4f03
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-26 11:23:53 -0500

    reduce function

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  var newList = [];
-  for (var i = list.length - 1; i >= 0; i--) {
-    newList += list[i];
-  }
-  return newList;
-}
+    return list.reduce((newList, item) => [item].concat(newList), []);
+  };
