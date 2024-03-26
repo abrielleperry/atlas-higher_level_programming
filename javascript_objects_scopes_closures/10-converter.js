c5bb2ba087f80bc151d82bf0b5ae4ed2f1e1d846
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-26 16:35:34 -0500

    semi fix

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -2,9 +2,9 @@
 exports.converter = function (base) {
   return function (number) {
     return number.toString(base);
-  }
-}
- // returns func that takes number as arg
- // then converts number from base 10 to base specified
- // by calling tostring method on number and passing base as argument
- // tostring converts number to string in base specified
+  };
+};
+// returns func that takes number as arg
+// then converts number from base 10 to base specified
+// by calling tostring method on number and passing base as argument
+// tostring converts number to string in base specified
