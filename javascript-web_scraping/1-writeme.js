48203d908063cc0bec10b02f395f1b23903ea4eb
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-28 14:00:34 -0500

    semifix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const filePath = process.argv[2];
-const string = process.argv[3];
-fs.write(filePath, 'utf8', (err, data) => {
+const filePath = process.argv[2]; // in this file
+const string = process.argv[3]; // put this string in this^ file
+fs.writeFile(filePath, string, 'utf8', (err) => {
   if (err) {
-  console.log(err)
-  } else {
-    console.log(data)
+    console.log(err);
   }
 });
