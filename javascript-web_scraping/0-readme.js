f13dbc3c931eda24c230185fdc93d6fdad2572fd
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-28 13:08:44 -0500

    handle errors

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const read_content = require('fs');
-const file_path = process.argv[2];
+const fs = require('fs');
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf8', (error, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(body);
+  }
+});
