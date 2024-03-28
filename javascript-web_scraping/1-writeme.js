e477a1b66c6fd4fdb987f4547a70a4b78601d17c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-28 13:55:41 -0500

    adding take arg 3 for string

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const string = process.argv[3];
+fs.write(filePath, 'utf8', (err, data) => {
+  if (err) {
+  console.log(err)
+  } else {
+    console.log(data)
+  }
+});
