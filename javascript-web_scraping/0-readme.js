b359a84fd7538da7fa96b0978045a5f37fb21163
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-28 13:12:54 -0500

    data

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf8', (error, body) => {
+fs.readFile(filePath, 'utf8', (error, data) => {
   if (error) {
     console.log(error);
   } else {
-    console.log(body);
+    console.log(data);
   }
 });
+
+//readFile(path, options, callback)
+// path- if both file and program are insame folder then simply give name of text file
+// options- 
