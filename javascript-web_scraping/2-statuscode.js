eeb8fe2d415898e079390d6ca626129ffdddf1fd
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-28 16:50:34 -0500

    fix get request w func

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request')
 const url = process.argv[2];
-if (request.get(url, error, response) {
+request.get(url, function (error, response) {
   console.log(error)
 });
