242a5a7fbf4dc86d17d0f299fdd3197e6f8c0076
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-29 11:21:45 -0500

    error handle

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 const apiUrl = process.argv[2];
+request.get(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    
+  }
+}
