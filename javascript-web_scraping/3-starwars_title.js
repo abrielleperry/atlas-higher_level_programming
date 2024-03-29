8c8bb54ad2693c51d879d3fad65768f7d586c715
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-29 10:37:33 -0500

    convert data to json obj

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,10 @@
 const movie_ID = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + movie_ID;
-request.get(url, function (error, response, body) { // body is the data from the request(api)
+request.get(url, function (error, response, body) { // body: the data from the request(api)
   if (error) {
     console.error('error:', error);
   } else {
+    console.log(JSON.parse(body).title);
+  }
+});
