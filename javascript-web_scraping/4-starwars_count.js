a401c9fd6ad7f2ca0529775f4ab191bb50273a43
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-29 11:40:58 -0500

    go throughs films until find 18

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,6 +6,12 @@ request.get(apiUrl, function (error, response, body) {
     console.error('error:', error);
   } else {
     let counter = 0;
-    const films = JSON.parse(body).results;
-  }
-}
+    const films = JSON.parse(body).results; // turn json string into javascript objects (array)
+      for (const film of films) {
+        if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+          counter++;
+        }
+      }
+      console.log(counter);
+    }
+  });
