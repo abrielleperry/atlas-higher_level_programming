e5c009ff81ff50f719152e43f7ee2cebdd0b92a7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-29 10:35:06 -0500

    get data and handle error from request

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,3 +2,7 @@
 const movie_ID = process.argv[2];
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + movie_ID;
+request.get(url, function (error, response, body) { // body is the data from the request(api)
+  if (error) {
+    console.error('error:', error);
+  } else {
