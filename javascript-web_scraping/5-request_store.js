e6c83c871f157b3211d7b10cefba77e4c0339b86
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-30 13:04:01 -0500

    adding notes

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -3,12 +3,12 @@ const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
 const fileName = process.argv[3];
-request.get(url, function (error, response, body) { 
+request.get(url, function (error, response, body) { // starts get request to url
 if (error) {
   console.error('error:', error);
-  return;
+  return; // stops execution of the function if error happens
 }
-  fs.writeFile(fileName, body, 'utf8', function (error) {
+  fs.writeFile(fileName, body, 'utf8', function (error) { // writes body to file w utf8 encoding
     if (error) {
       console.error('error:', error);
     }
