7f3a3923c1ff84c6c3a90289f1065de4f39f96a1
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-30 13:11:42 -0500

    error handling

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -2,4 +2,7 @@
 const request = require('request');
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 request.get(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  }
 });
