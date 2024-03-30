f3d4f626ae4fd61a547829b9a40f4e30862e17b7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-03-30 12:55:12 -0500

    error handling

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,4 +4,7 @@ const fs = require('fs');
 const url = process.argv[2];
 const fileName = process.argv[3];
 request.get(url, function (error, response, body) {
+if (error) {
+  console.error('error:', error);
 }
+});
