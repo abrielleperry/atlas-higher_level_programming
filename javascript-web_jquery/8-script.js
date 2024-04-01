986c2e18b27b245ef6703821165b3fde2c62e07c
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-04-01 17:21:43 -0500

    append new list to ul movie element title

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,5 +1,6 @@
 const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 $.get(url, function (data, status) {
   data.results.forEach(function (movie) {
-  $('UL#list_movies').text(data.title);
+    $('UL#list_movies').append('<li>' + movie.title + '</li>');
+  });
 });
