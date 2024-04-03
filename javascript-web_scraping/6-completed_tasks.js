9904a29e4563f9b9258df595b86b5f74abb81007
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-04-02 20:05:50 -0500

    stop if error

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const request = require('request');
-const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+const request = require("request");
+const apiUrl = "https://jsonplaceholder.typicode.com/todos";
 request.get(apiUrl, function (error, response, body) {
   if (error) {
-    console.error('error:', error);
+    console.error("error:", error);
+    return;
   }
 });
